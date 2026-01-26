705ad8254841c34ef33b86a12917fac23be96e3b
Nonso <69207786+skepticaldude@users.noreply.github.com>
2026-01-26 10:49:17 +0100

    Implement odd/even checker for a range of numbers
    Add a loop to print whether numbers are odd or even.

---
+++ loop_python.py
@@ -0,0 +1,9 @@
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+for num in numbers:
+    if num % 2 == 0:
+        print(f"{num} is even")
+    else:
+        print(f"{num} is odd")
+
+  #this is showing how loops work and how it outputs 
+  #it displays numbers to show wether they are odd and even  
